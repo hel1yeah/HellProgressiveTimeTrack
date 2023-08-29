0047ade521e5ceb2d2cf4884449e76bc47d1104d
hel1_yeah <50619201+hel1yeah@users.noreply.github.com>
2023-08-29 17:30:41 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<template>
+  <header class="sticky top-0 z-50 flex items-center justify-between border-b bg-white p-3">
+    <a href="#">
+      <img src="./assets/logo.png" alt="logo" class="h-9" />
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        Day Template
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>Progress : <span class="fonts-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-600"></div>
+      </div>
+    </a>
+  </header>
+
+  <main class="flex flex-grow flex-col"></main>
+
+  <nav class="sticky bottom-0 z-50 bag bg-white text-3x1">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#timeline">
+          <ClockIcon class="h-6 w-6" />
+          TimeLine</a
+        >
+      </li>
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#activites">
+          <ListBulletIcon class="h-6 w-6" />
+          activites</a
+        >
+      </li>
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#progress">
+          <ChartBarIcon class="h-6 w-6" />
+          progress</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
