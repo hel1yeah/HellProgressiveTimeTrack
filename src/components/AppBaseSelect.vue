18ded7d1e97785ba9c67981040a32244d52bc5a3
hel1_yeah <hel1_yeah@ukr.net>
2023-11-22 20:44:55 +0200

    fix: base

---
+++ src/components/AppBaseSelect.vue
@@ -0,0 +1,40 @@
+<script setup>
+import AppBaseButton from '@/components/AppBaseButton.vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+
+const props = defineProps({
+  placeholder: {
+    type: String,
+    required: false,
+    default: 'Rest'
+  },
+  selected: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  options: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="flex gap-2">
+    <AppBaseButton>
+      <XMarkIcon class="h-8" />
+    </AppBaseButton>
+    <select class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl">
+      <option selected disabled value="">{{ placeholder }}</option>
+      <option
+        v-for="{ value, label } in props.options"
+        :key="value"
+        :value="value"
+        :selected="value === props.selected"
+      >
+        {{ label }}
+      </option>
+    </select>
+  </div>
+</template>
